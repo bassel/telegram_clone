9c158c82888bccd44fa0eea4598a4f328ee13690
Bassel <bassel.bc@gmail.com>
2019-03-17 16:29:07 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Telegram Clone',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Telegram')),
+        body: Center(child: Text('Telegram')),
+      ),
+    );
+  }
+}
